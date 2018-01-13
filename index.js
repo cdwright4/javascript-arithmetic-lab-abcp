f7c46eada56dd694ad7e7b526189ecbc319f0578
Crystal <crystal_wright@yahoo.com>
2018-01-13 20:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ function add (num1, num2) {
   return a + b;
 }
 
+function subtract(num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a - b;
+}
+
+function multiply (num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a * b;
+}
+
+function divide (num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a/b;
+}
+
+function inc(num1) { 
+  return ++num1;
+}
